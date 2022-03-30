e65ea22a9307098606ce7163660cd31d40e032d3
Nicky Donna <nickydonna@fastmail.com>
2022-03-30 11:33:50 -0300

    ignore pg-native

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    return {
+      ...config,
+      plugins: [
+        ...config.plugins,
+        new webpack.IgnorePlugin({ resourceRegExp: /pg-native/ })
+      ]
+    }
+  }
 }
 
 module.exports = nextConfig
